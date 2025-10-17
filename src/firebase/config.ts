9ac6d7d79321b7194a5f278fbb89b83ea1acbd18
Santiago Miranda <santiago@vidana.com.mx>
2025-10-17 19:53:39 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2129569698-d805d",
+  "appId": "1:650074038837:web:1b73aa8f5188f65b2b077c",
+  "apiKey": "AIzaSyDHgHHmFJiNGH3LVRaq9tqwchCpx8d71QY",
+  "authDomain": "studio-2129569698-d805d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "650074038837"
+};

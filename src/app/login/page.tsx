9de40d5990f48bd8d3ce4366ffa4732a797100ed
Santiago Miranda <santiago@vidana.com.mx>
2025-12-04 02:02:38 +0000

    Try fixing this error: `Runtime Error: Error: useEffect is not defined.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, type FC } from 'react';
+import { useState, useEffect, type FC } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useAuth, useUser } from '@/firebase';
